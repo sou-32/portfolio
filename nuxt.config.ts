c05c742bc28e8683196ebbcbb9c59eb33bbaf0c3
sou-32 <108719784+sou-32@users.noreply.github.com>
2023-04-17 18:55:00 +0900

    hello

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,13 @@ export default defineNuxtConfig({
       assetsEndpoint: "",
     },
   },
+  app: {
+    head: {
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
 });
---
+++ pages/works/[name].vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import { getAssets } from "~/src/model/constant";
+
+interface Works {
+  name: string;
+  description: string;
+  works: {
+    extension: string;
+  };
+}
+
+const works: Works[] = [
+  {
+    name: "table",
+    description: "table",
+    works: {
+      extension: "mp4",
+    },
+  },
+  {
+    name: "chill-out",
+    description: "chill-out",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "fukakouryoku",
+    description: "fukakouryoku",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "hello",
+    description: "hello",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "light-saver",
+    description: "light-saver",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "m5stack",
+    description: "m5stack",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "manbou",
+    description: "manbou",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "manbou-typing",
+    description: "manbou-typing",
+    works: { extension: "mp4" },
+  },
+  {
+    name: "suzannu",
+    description: "suzannu",
+    works: { extension: "mp4" },
+  },
+];
+
+function findWorkByName(works: Works[], name: string): Works | undefined {
+  return works.find((work) => work.name === name);
+}
+
+let name = ref<string>("hello");
+onMounted(() => {
+  name.value = window.location.pathname.split("/").pop()!;
+});
+const currentWork = findWorkByName(works, name.value)!;
+const getTmbSrc = getAssets(`projects/${currentWork.name}/tmb.png`);
+const getVideoSrc = getAssets(
+  `projects/${currentWork.name}/works.${currentWork.works.extension}`
+);
+</script>
+
+<template>
+  {{ currentWork.name }}<br />
+  <img :src="getTmbSrc" height="300px" /><br />
+  <video :src="getVideoSrc" height="300px" controls></video>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  background-color: aqua;
+}
+
+.date {
+  background-color: gray;
+}
+
+.body {
+  font-size: larger;
+}
+</style>
